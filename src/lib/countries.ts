ef5fc03ae5ee2710c1446bc75476691c17ec90a2
ousseynoudanfa6-oss <ousseynoudanfa6@gmail.com>
2026-02-24 18:03:15 +0000

    Create countries data file for African West countries

---
+++ src/lib/countries.ts
@@ -0,0 +1,18 @@
+export const africanWestCountries = [
+  { code: 'BJ', name: 'Benin', flag: '🇧🇯', phoneCode: '+229', phoneFormat: '0 XX XX XX XX' },
+  { code: 'BF', name: 'Burkina Faso', flag: '🇧🇫', phoneCode: '+226', phoneFormat: '0 XX XX XX XX' },
+  { code: 'CV', name: 'Cabo Verde', flag: '🇨🇻', phoneCode: '+238', phoneFormat: '0 XX XX XX XX' },
+  { code: 'CI', name: 'Côte d'Ivoire', flag: '🇨🇮', phoneCode: '+225', phoneFormat: '0 XX XX XX XX' },
+  { code: 'GH', name: 'Ghana', flag: '🇬🇭', phoneCode: '+233', phoneFormat: '0 XX XXX XXXX' },
+  { code: 'GM', name: 'Gambia', flag: '🇬🇲', phoneCode: '+220', phoneFormat: '0 XX XXX XXXX' },
+  { code: 'GN', name: 'Guinea', flag: '🇬🇳', phoneCode: '+224', phoneFormat: '0 XX XX XX XX' },
+  { code: 'GW', name: 'Guinea-Bissau', flag: '🇬🇼', phoneCode: '+245', phoneFormat: '0 XX XX XX XX' },
+  { code: 'LR', name: 'Liberia', flag: '🇱🇷', phoneCode: '+231', phoneFormat: '0 XX XXXX' },
+  { code: 'ML', name: 'Mali', flag: '🇲🇱', phoneCode: '+223', phoneFormat: '0 XX XX XX XX' },
+  { code: 'MR', name: 'Mauritania', flag: '🇲🇦', phoneCode: '+222', phoneFormat: '0 XX XX XX XX' },
+  { code: 'NE', name: 'Niger', flag: '🇳🇪', phoneCode: '+227', phoneFormat: '0 XX XX XX XX' },
+  { code: 'NG', name: 'Nigeria', flag: '🇳🇬', phoneCode: '+234', phoneFormat: '0 XX XXXX XXXX' },
+  { code: 'SN', name: 'Senegal', flag: '🇸🇳', phoneCode: '+221', phoneFormat: '0 XX XX XX XX' },
+  { code: 'SL', name: 'Sierra Leone', flag: '🇸🇱', phoneCode: '+232', phoneFormat: '0 XX XXXX' },
+  { code: 'TG', name: 'Togo', flag: '🇹🇬', phoneCode: '+228', phoneFormat: '0 XX XX XX XX' }
+];
